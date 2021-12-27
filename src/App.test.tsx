c2fc4f42d1b3a38deb5b183975d6167115026901
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2021-12-27 13:49:41 +0000

    added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,41 @@
-/* eslint-disable */
-import React from "react";
-import { cleanup, render, screen } from "@testing-library/react";
+/*eslint-disable */
+import {
+  cleanup,
+  getByLabelText,
+  render,
+  screen,
+} from "@testing-library/react";
 import { App } from "./App";
+import { DropDown } from "./components/SearchResultsContainer";
+import { SearchResults } from "./components/SearchResultsContainer/SearchResults";
+import renderer from "react-test-renderer";
+import { SearchBox } from "./components/SearchBox";
+import { Footer } from "./components/Footer";
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
 
 afterEach(() => {
   cleanup();
 });
 
-it("should render h1", () => {
-  render(<App />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument;
+describe("App component", () => {
+  it("should render App component", () => {
+    render(<App />);
+    expect(<App />).toBeInTheDocument;
+  });
 });
+describe("SearchResults component", () => {
+
+  it("should render the placeholder input", () => {
+    render(<SearchResults placeholder={"Pick-up Location"} />);
+    const placeholderInput = screen.getByRole("textbox");
+    expect(placeholderInput).toBeInTheDocument;
+  });
+
+  it("should render the placeholder value into placeholder text", () => {
+    render(<SearchResults placeholder="Pick-up Location" />);
+    const placeholderText = screen.getByPlaceholderText(/Pick-up Location/i);
+    expect(placeholderText).toBeInTheDocument;
+  });
+});
+
