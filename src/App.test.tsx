69a7f831fadde25a5b10f256d188b35c8e44109f
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2021-12-27 13:57:35 +0000

    added tests and snapshots

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -39,3 +39,106 @@ describe("SearchResults component", () => {
   });
 });
 
+describe("DropDown component", () => {
+  it("should render the listbox and an option", () => {
+    render(
+      <DropDown
+        locations={[
+          {
+            bookingId: "airport-38566",
+            city: "Manchester",
+            country: "United Kingdom",
+            iata: "MAN",
+            name: "Manchester Airport",
+            region: "Greater Manchester",
+          },
+        ]}
+      />
+    );
+    const dropdownList = screen.getByRole("listbox");
+    expect(dropdownList).toBeInTheDocument;
+    const dropdownOption = screen.getByRole("option");
+    expect(dropdownOption).toBeInTheDocument;
+  });
+});
+
+describe("Footer component", () => {
+  it("should render Footer component", () => {
+    render(<Footer />);
+    expect(<Footer />).toBeInTheDocument;
+  });
+});
+
+describe("accesibility tests", () => {
+  it("should have an input label", () => {
+    render(<SearchResults placeholder={"Pick-up Location"} />);
+    const placeholderLabel = screen.getByLabelText("Pick-up Location");
+    expect(placeholderLabel).toBeInTheDocument;
+  });
+
+  it("should have a footer label", () => {
+    render(<Footer />);
+    const footerLabel = screen.getByLabelText("Information Footer");
+    expect(footerLabel).toBeInTheDocument;
+  });
+
+  it("should have a dropdown list label", () => {
+    render(
+      <DropDown
+        locations={[
+          {
+            bookingId: "airport-38566",
+            city: "Manchester",
+            country: "United Kingdom",
+            iata: "MAN",
+            name: "Manchester Airport",
+            region: "Greater Manchester",
+          },
+        ]}
+      />
+    );
+    const dropdownLabel = screen.getByLabelText("Search Result List");
+    expect(dropdownLabel).toBeInTheDocument;
+  });
+  it("should have no accesibility errors on app component", async () => {
+    const { container } = render(<App />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+  it("should have no accesibility errors on Footer component", async () => {
+    const { container } = render(<Footer />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+  it("should have no accesibility errors on SearchResults component", async () => {
+    const { container } = render(
+      <SearchResults placeholder={"Pick-up Location"} />
+    );
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
+
+describe("Snapshot tests", () => {
+  it("should match the snapsho of the App componentt", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("should match the snapshot of the SearchBox component", () => {
+    const tree = renderer.create(<SearchBox />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("should match the snapshot of the DestinationType component", () => {
+    const tree = renderer
+      .create(<SearchResults placeholder={"Pick-up Location"} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("should match the snapshot of the Footer component", () => {
+    const tree = renderer.create(<Footer />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
